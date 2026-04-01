afdac6d06a059301cbc21c65a11359e62f5b7b94
Harshi-1401 <harshisvc@gmail.com>
2026-04-01 23:18:24 +0530

    Fix CORS for Vercel frontend

--- python_backend/main.py
+++ python_backend/main.py
@@ -32,7 +32,10 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=[
+        "http://localhost:5173",
+        "https://time-capsule-six-coral.vercel.app",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
